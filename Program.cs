b9e12bdcd5a70fecb60f0e67f55b73e6be1362b6
Toomas Lepplaan <lepplaan@gmail.com>
2024-09-03 16:42:02 +0300

    Create new app

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Rakendus küsib kasutajal sisestada tema nime
+// Rakendus tervitab kasutajat nimepidi
+Console.WriteLine("Enter you name:"); //Output
+
+// string - sõne
+string userName = Console.ReadLine(); //Input
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //Output
+// string interpolation
+
+Console.WriteLine($"Hello, {userName}!"); //Output
